0e9d9b5374f8f2f1ac2ed80bb94afc3beeb031f0
muriloms <ms7murilo@gmail.com>
2022-01-11 15:53:26 -0300

    add codes section 3

---
+++ Sec3_CarRacing/Assets/Scripts/SimpleAICar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleAICar : MonoBehaviour {
+
+	public Transform goal;
+	public Text readout;
+	private float acceleration = 5f;
+	private float deceleration = 5f;
+	private float minSpeed = 0.0f;
+	private float maxSpeed = 100.0f;
+	private float breakAngle = 20f;
+	private float speed = 0.0f;
+	public float rotSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 lookAtGoal = new Vector3(goal.position.x, 
+										this.transform.position.y, 
+										goal.position.z);
+		Vector3 direction = lookAtGoal - this.transform.position;
+
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+												Quaternion.LookRotation(direction), 
+												Time.deltaTime*rotSpeed);
+
+		if (Vector3.Angle(goal.forward, transform.forward) > breakAngle && speed > 10)
+		{
+			speed = Mathf.Clamp(speed - (deceleration * Time.deltaTime), minSpeed, maxSpeed);
+		}
+		else
+		{
+			speed = Mathf.Clamp(speed + (acceleration * Time.deltaTime), minSpeed, maxSpeed);
+		}
+		
+		this.transform.Translate(0,0,speed);
+		AnalogueSpeedConverter.ShowSpeed(speed, minSpeed, maxSpeed);
+		if(readout) readout.text = "" + (int)speed;
+	}
+}
